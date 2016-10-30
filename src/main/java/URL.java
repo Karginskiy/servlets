b2dd96e07c41cbe7a0c205e2c06abaa014720873
Kargin Nikita <hei@spark-mail.ru>
2016-10-30 16:33:08 +0300

    URL annotation validator

---
+++ src/main/java/URL.java
@@ -0,0 +1,18 @@
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = {URLValidator.class})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface URL {
+    String message() default "Malformed URL";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    String protocol() default "";
+    String host() default "";
+    int port() default -1;
+}
