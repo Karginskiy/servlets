45c5dd438597cdcc14bba1dd49af2f4d921dcd70
Kargin Nikita <hei@spark-mail.ru>
2016-10-30 16:32:31 +0300

    Validator of email

---
+++ src/main/java/Email.java
@@ -0,0 +1,22 @@
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@NotNull
+@Size(min = 7)
+@Pattern(regexp = "[a-z0-9!#$%&'*+/=?^ `{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*" +
+                  "@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\\\.)+[a-z0-9](?:[a-z0-9-]*" +
+                  "[a-z0-9])?")
+@Constraint(validatedBy = {})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Email {
+    String message() default "Неверный электронный адрес";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
